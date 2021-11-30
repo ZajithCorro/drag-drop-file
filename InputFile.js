70734795aca75f05977088df2a18d662fbfafef1
Zajith Corro <zajithcorrov@hotmail.com>
2021-11-29 18:02:30 -0600

    :sparkles: feat: check if there's one file to match with list of files

--- InputFile.js
+++ InputFile.js
@@ -1,19 +1,27 @@
+const filesExtensions = {
+	jpg: 'image/jpeg',
+	png: 'image/png',
+	pdf: 'application/pdf',
+};
+
 class InputFile {
 	constructor(selector, config) {
-		this.selectorContainer = document.querySelector(selector);
-		this.btnAuxFile = document.querySelector(`${selector} #file-btn`);
-		this.inputFile = document.querySelector(`${selector} #file-input`);
+		this._selectorContainer = document.querySelector(`${selector} .input-file-dragzone`);
+		this._btnAuxFile = document.querySelector(`${selector} #file-btn`);
+		this._inputFile = document.querySelector(`${selector} #file-input`);
+		this._listFilesToMatch = config.files;
+		this.errors = [];
+		this.validateFiles = [];
 
 		this.init();
 	}
 
 	init() {
-		console.log('😎');
-		this.selectorContainer.addEventListener('dragover', this._dragOverEvent.bind(this));
-		this.selectorContainer.addEventListener('dragenter', this._dragEnterEvent.bind(this));
-		this.selectorContainer.addEventListener('dragleave', this._dragLeaveEvent.bind(this));
-		this.selectorContainer.addEventListener('drop', this._dragDropEvent.bind(this));
-		this.btnAuxFile.addEventListener('click', this._clickBtnAuxEvent.bind(this));
+		this._selectorContainer.addEventListener('dragover', this._dragOverEvent.bind(this));
+		this._selectorContainer.addEventListener('dragenter', this._dragEnterEvent.bind(this));
+		this._selectorContainer.addEventListener('dragleave', this._dragLeaveEvent.bind(this));
+		this._selectorContainer.addEventListener('drop', this._dragDropEvent.bind(this));
+		this._btnAuxFile.addEventListener('click', this._clickBtnAuxEvent.bind(this));
 	}
 
 	_dragOverEvent(event) {
@@ -22,22 +30,50 @@ class InputFile {
 	}
 
 	_dragEnterEvent(event) {
-		this.selectorContainer.classList.add('active');
+		this._selectorContainer.classList.add('active');
 	}
 
 	_dragLeaveEvent(event) {
-		this.selectorContainer.classList.remove('active');
+		this._selectorContainer.classList.remove('active');
 	}
 
 	_dragDropEvent(event) {
 		event.preventDefault();
 
-		this.selectorContainer.classList.remove('active');
-		console.log(event.dataTransfer);
+		this._resetErrors();
+		const files = Array.from(event.dataTransfer.files);
+		this._selectorContainer.classList.remove('active');
+
+		this._checkFilesCorrect(files);
+
+		console.log(this.errors);
 	}
 
 	_clickBtnAuxEvent() {
-		this.inputFile.click();
+		this._inputFile.click();
+	}
+
+	_resetErrors() {
+		this.errors = [];
+	}
+
+	_checkFilesCorrect(listOfFiles) {
+		if (this._listFilesToMatch.lenght === 0) return true;
+
+		this._listFilesToMatch.forEach((element) => {
+			if (element.required) {
+				const fileExists = listOfFiles.some((file) => {
+					const fileName = file.name.split('.')[0];
+					const fileType = file.type;
+
+					return fileName === element.name && filesExtensions[element.type] === fileType;
+				});
+
+				if (!fileExists) {
+					this.errors.push(`El archivo ${element.name} es requerido`);
+				}
+			}
+		});
 	}
 }
 
