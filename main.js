8bfb3dcbfb691be652612e783e7a9c9ad03a6faf
Zajith Corro <zajithcorrov@hotmail.com>
2021-11-29 18:03:44 -0600

    :sparkles: feat: calling the new class

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import InputFile from './InputFile.js';
 
-const inputFile = new InputFile('#input-file-alta');
+const inputFile = new InputFile('#input-file-alta', {
+	files: [
+		{ name: 'ACTA_NACIMIENTO', type: 'pdf', required: true },
+		{ name: 'ACTA_MATRIMONIO', type: 'pdf', required: true },
+	],
+});
