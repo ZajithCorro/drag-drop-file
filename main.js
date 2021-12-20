adfe4b01024dea141dd81c08a3b8fbc339a33f71
Zajith Corro <zajithcorrov@hotmail.com>
2021-12-19 23:12:42 -0600

    :sparkles: feat: Configuration file to the class

--- main.js
+++ main.js
@@ -4,5 +4,13 @@ const inputFile = new InputFile('#input-file-alta', {
 	files: [
 		{ name: 'ACTA_NACIMIENTO', type: 'pdf', required: true },
 		{ name: 'ACTA_MATRIMONIO', type: 'pdf', required: true },
+		{ name: 'ACTA_DEFUNCION', type: 'pdf', required: true },
+		{ name: 'FOTOGRAFIA_FRENTE', type: 'png', required: true },
 	],
+});
+
+const btnEnviar = document.querySelector('#btn-enviar');
+
+btnEnviar.addEventListener('click', () => {
+	console.log(inputFile.files);
 });
